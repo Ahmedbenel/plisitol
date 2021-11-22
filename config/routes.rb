e6d5a77215cf5a5166bbfceb64c84391d1363cc5
Ahmed BenElHajHamza <benelhajhamza.ahmed@gmail.com>
2021-11-22 17:47:33 +0100

    creation controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :programs, only: [:index, :show] do
+    resources :reviews, only: :create
+  end
+  get "/dashboard", to: 'dashboard#show', as: :dashboard
 end
