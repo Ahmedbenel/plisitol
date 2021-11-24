a7929218b5fe7f4297fc794f1e7a18bb06d74d14
Perrine Torcol <perrine.trcl@gmail.com>
2021-11-24 18:08:40 +0100

    search form done but need front-end and age correction : can only be filtered by min age

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,4 +1,8 @@
 class Program < ApplicationRecord
+  CATEGORY = ["documentaire", "comédie", "animation", "educatif", "aventure", "fantastique", "action"]
+  AGE = ["0-5", "5-10", "10-15"]
+  LENGTH = ["0-25", "25-50", "50-100", "100-200"]
+
   belongs_to :platform
   has_many :watchings
   has_many :favorites
