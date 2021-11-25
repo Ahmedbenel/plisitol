980f21fab41da7277a07915e966c93f4e00a4582
Ahmed BenElHajHamza <benelhajhamza.ahmed@gmail.com>
2021-11-25 15:13:36 +0100

    review done

--- app/models/program.rb
+++ app/models/program.rb
@@ -5,6 +5,7 @@ class Program < ApplicationRecord
 
   belongs_to :platform
   has_many :watchings
+  has_many :reviews, through: :watchings
   has_many :favorites
   validates :title, presence: true, uniqueness: true
   has_one_attached :photo
