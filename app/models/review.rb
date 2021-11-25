bb998f20a8d2794f53c92c584ac505c1abbc6e2a
Ahmed BenElHajHamza <benelhajhamza.ahmed@gmail.com>
2021-11-25 12:20:40 +0100

    review form done with JS

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
+  AUTHORIZED_RATINGS = (1..5)
   belongs_to :watching
   validates :content, presence: true
-  validates :rating, numericality: { in: 0..5 }
+  validates :rating, inclusion: { in: AUTHORIZED_RATINGS }
 end
