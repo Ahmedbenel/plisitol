41b790a8fc16d2840ac0777179af5e1819acb03e
Perrine Torcol <perrine.trcl@gmail.com>
2021-11-25 22:26:20 +0100

    squeleton for the dashboard show

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,26 @@
 class AccountsController < ApplicationController
   def new
+    @account = Account.new
   end
 
   def create
+    @account = Account.create(account_params)
+    @account.user = current_user
+    @account.save
+    redirect_to dashboard_path(@account)
   end
 
   def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    respond_to do |format|
+      format.html { redirect_to dashboard_url notice: 'Compte supprimé avec succès.'}
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:platform)
   end
 end
