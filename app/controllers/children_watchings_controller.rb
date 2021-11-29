67ecb5233a0be76552ddde27fd7ddb304c17000c
Lauriane NATHOU <laurianenathou@gmail.com>
2021-11-29 14:58:28 +0100

    Watching create and new

--- app/controllers/children_watchings_controller.rb
+++ app/controllers/children_watchings_controller.rb
@@ -4,6 +4,20 @@ class ChildrenWatchingsController < ApplicationController
     @watching = Watching.find(params[:watching_id])
   end
 
-  # def create
-  # end
+  def create
+    @children_watching = ChildrenWatching.new
+    @watching = Watching.find(params[:watching_id])
+    @children_watching.watching = @watching
+    @child = Child.find(params[:child_id])
+    @children_watching.child = @child
+    if @children_watching.save
+      redirect_to new_watching_children_watching_path(@children_watching)
+    end
+  end
+
+  def destroy
+    @children_watching = ChildrenWatching.find(params[:id])
+    @children_watching.destroy
+    redirect_to programs_path
+  end
 end
