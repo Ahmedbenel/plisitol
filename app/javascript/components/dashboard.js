7f8ababad12e358abd73f8aa5a869f92659d5902
Perrine Torcol <perrine.trcl@gmail.com>
2021-11-29 17:24:10 +0100

    initialisation for stats with highcharts

--- app/javascript/components/dashboard.js
+++ app/javascript/components/dashboard.js
@@ -12,3 +12,37 @@ const addChild = () => {
 }
 
 export { addChild }
+
+
+
+
+
+
+
+const myTestHighCharts = document.addEventListener('DOMContentLoaded', function () {
+  const chart = Highcharts.chart('container', {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      }
+    },
+    series: [{
+      name: 'Jane',
+      data: [1, 0, 4]
+    }, {
+      name: 'John',
+      data: [5, 7, 3]
+    }]
+  });
+});
+
+export { myTestHighCharts }
