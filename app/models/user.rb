4ae8792c0614ddc1dd617429c88b1ec8a0833e1c
Lauriane NATHOU <laurianenathou@gmail.com>
2021-11-29 09:49:51 +0100

    Add dependent destroy on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :accounts
-  has_many :watchings
-  has_many :children
-  has_many :favorites
+  has_many :accounts, dependent: :destroy
+  has_many :watchings, dependent: :destroy
+  has_many :children, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_many :platforms, through: :accounts
   has_many :reviews, through: :watchings
   has_one_attached :photo
