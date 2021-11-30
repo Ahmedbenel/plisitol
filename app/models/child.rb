59d7d07f5b3014d3e6579214f9d719659ea2f009
Lauriane NATHOU <laurianenathou@gmail.com>
2021-11-30 10:48:41 +0100

    redirect to platform from children watching done

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,4 +4,8 @@ class Child < ApplicationRecord
   has_many :watchings, through: :children_watchings
   validates :name, presence: true, uniqueness: true
   has_one_attached :photo
+
+  def find_child_watchings(watching)
+    self.children_watchings.find_by(watching: watching)
+  end
 end
